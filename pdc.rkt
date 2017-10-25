1ac141ed20d62a9fd63aafa211a43b37eaebaf82
Nickotronz7 <nickolasrc7@gmail.com>
2017-10-25 01:59:19 -0600

    Posibles posiciones del caballo y funcion elimina ultimo elemento

--- pdc.rkt
+++ pdc.rkt
@@ -2,6 +2,7 @@
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname pdc) (
   read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ; Función que crea el tablero
 (define (crear_Tablero rows&cols)(
   cond ((equal? rows&cols 1) '(0))(
@@ -51,8 +52,8 @@
 ;Funcion que obtiene el valor de una lista
 (define (get_Val matrix row col)(
   cond ((not (equal? row 0)) (get_Val (cdr matrix) (- row 1) col))
-       ((and (equal? row 0) (not (equal? col 0))) (get_ValAux col (car matrix)))(
-         else (caar matrix)
+       ((and (equal? row 0) (not (equal? col 0))) (get_ValAux col (car matrix)))
+       (else (caar matrix)
          )
   ))
 
@@ -63,6 +64,60 @@
     )
   ))
 
+;Funcion que devuelve una matriz con los posibles movimientos  del caballo
+(define (generate_legal_moves actual posible_movs size step)(
+  cond ((or (>= (car actual) size) (>= (cadr actual) size) (< (car actual) 0)
+    (< (cadr actual) 0)) '())
+       ((>= step size) posible_movs)
+       ((and (equal? step 0) (<= 0 (+ (car actual) 1))
+         (<= 0 (+ (cadr actual) 2)) (< (+ (car actual) 1) size)
+           (< (+ (cadr actual) 2) size)) (generate_legal_moves actual
+             (append posible_movs (list '(1 2))) size (+ step 1)))
+       ((and (equal? step 1) (<= 0 (+ (car actual) 1))
+         (<= 0 (- (cadr actual) 2)) (< (+ (car actual) 1) size)
+           (< (- (cadr actual) 2) size)) (generate_legal_moves actual
+             (append posible_movs (list '(1 -2))) size (+ step 1)))
+       ((and (equal? step 2) (<= 0 (- (car actual) 1))
+         (<= 0 (+ (cadr actual) 2)) (< (- (car actual) 1) size)
+           (< (+ (cadr actual) 2) size)) (generate_legal_moves actual
+             (append posible_movs (list '(-1 2))) size (+ step 1)))
+       ((and (equal? step 3) (<= 0 (- (car actual) 1))
+         (<= 0 (- (cadr actual) 2)) (< (- (car actual) 1) size)
+           (< (- (cadr actual) 2) size)) (generate_legal_moves actual
+             (append posible_movs (list '(-1 -2))) size (+ step 1)))
+       ((and (equal? step 4) (<= 0 (+ (car actual) 2))
+         (<= 0 (+ (cadr actual) 1)) (< (+ (car actual) 2) size)
+           (< (+ (cadr actual) 1) size)) (generate_legal_moves actual
+             (append posible_movs (list '(2 1))) size (+ step 1)))
+       ((and (equal? step 5) (<= 0 (+ (car actual) 2))
+         (<= 0 (- (cadr actual) 1)) (< (+ (car actual) 2) size)
+           (< (- (cadr actual) 1) size)) (generate_legal_moves actual
+             (append posible_movs (list '(2 -1))) size (+ step 1)))
+       ((and (equal? step 6) (<= 0 (- (car actual) 2))
+         (<= 0 (+ (cadr actual) 1)) (< (- (car actual) 2) size)
+           (< (+ (cadr actual) 1) size)) (generate_legal_moves actual
+             (append posible_movs (list '(-2 1))) size (+ step 1)))
+       ((and (equal? step 7) (<= 0 (- (car actual) 2))
+         (<= 0 (- (cadr actual) 1)) (< (- (car actual) 2) size)
+           (< (- (cadr actual) 1) size)) (generate_legal_moves actual
+             (append posible_movs (list '(-2 -1))) size (+ step 1)))(
+         else (generate_legal_moves actual posible_movs size (+ step 1))
+         )
+  ))
+
+;Funcion para eliminar el ultimo elemento de una lista
+(define (deLast lista lar)(
+  cond ((equal? lar 1) '())(
+    else (delAux lista lar '())
+    )
+  ))
+
+;Funcion auxiliar para eliminar el ultimo elemento
+(define (delAux lista lar res)(
+  cond ((equal? lar 1) res)(
+    else (delAux (cdr lista) (- lar 1) (append res (list (car lista))))
+    )
+  ))
 
 ;(define table '((1 2 3) (4 5 6) (7 8 9) (10 11 12) (13 14 15)))
 ;(define table (crear_Tablero 8))
